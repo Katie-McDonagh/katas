1f1cf725227534131b5037ae9366068de604ff72
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 17:00:59 +0000

    refactor redundant test

--- jasmine-standalone-3/spec/basicMathematicalOperations.js
+++ jasmine-standalone-3/spec/basicMathematicalOperations.js
@@ -1,9 +1,5 @@
 describe("basicOp", function(){
 
-  it("returns a number when given a number", function(){
-    expect(basicOp('', 1, 0)).toEqual(1)
-  });
-
   it("returns the numbers added together when give '+", function(){
     expect(basicOp('+', 1, 1)).toEqual(2)
   });
