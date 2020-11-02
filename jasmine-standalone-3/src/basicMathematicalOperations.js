f0461174f2bcfe88e61dc0c16bd6ad3fbbafd857
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 17:15:27 +0000

    refactor code to make one line solution

--- jasmine-standalone-3/src/basicMathematicalOperations.js
+++ jasmine-standalone-3/src/basicMathematicalOperations.js
@@ -1,21 +1,3 @@
-function basicOp(operation, value1, value2){
-  let result
-
-  // if(operation === '+'){
-  //   result = value1 + value2
-  // } else if(operation === '-') {
-  //   result = value1 - value2
-  // } else if(operation === '*') {
-  //   result = value1 * value2
-  // } else {
-  //   result = value1 / value2
-  // }
-  // return result
-
-  switch(operation){
-    case '+' : return value1 + value2
-    case '-' : return value1 - value2
-    case '*' : return value1 * value2
-    case '/' : return value1 / value2
-  }
+function basicOp(o, a, b) {
+  return eval(a+o+b);
 }
