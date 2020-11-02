c1583109752f11d975eddfc496e753015edc82d6
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 17:13:17 +0000

    refactor code

--- jasmine-standalone-3/src/basicMathematicalOperations.js
+++ jasmine-standalone-3/src/basicMathematicalOperations.js
@@ -1,14 +1,21 @@
 function basicOp(operation, value1, value2){
   let result
 
-  if(operation === '+'){
-    result = value1 + value2
-  } else if(operation === '-') {
-    result = value1 - value2
-  } else if(operation === '*') {
-    result = value1 * value2
-  } else {
-    result = value1 / value2
+  // if(operation === '+'){
+  //   result = value1 + value2
+  // } else if(operation === '-') {
+  //   result = value1 - value2
+  // } else if(operation === '*') {
+  //   result = value1 * value2
+  // } else {
+  //   result = value1 / value2
+  // }
+  // return result
+
+  switch(operation){
+    case '+' : return value1 + value2
+    case '-' : return value1 - value2
+    case '*' : return value1 * value2
+    case '/' : return value1 / value2
   }
-  return result
 }
