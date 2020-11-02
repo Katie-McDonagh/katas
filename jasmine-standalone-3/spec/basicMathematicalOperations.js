e741b1842fbd577c72b0285f25a034442cb6fcf3
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 17:07:40 +0000

    refactor tests

--- jasmine-standalone-3/spec/basicMathematicalOperations.js
+++ jasmine-standalone-3/spec/basicMathematicalOperations.js
@@ -1,18 +1,9 @@
 describe("basicOp", function(){
 
-  it("returns the numbers added together when give '+", function(){
+  it("returns the correct value according to the operation provided", function(){
     expect(basicOp('+', 1, 1)).toEqual(2)
-  });
-
-  it("returns the number that equals value 2 subtracted from value 1 when given '-'", function(){
     expect(basicOp('-', 5, 5 )).toEqual(0)
-  })
-
-  it("returns the numbers multiplied with each other", function(){
     expect(basicOp('*', 5, 5)).toEqual(25)
-  })
-
-  it("divides value1 by value2", function(){
     expect(basicOp('/', 49, 7)).toEqual(7)
   })
 });
